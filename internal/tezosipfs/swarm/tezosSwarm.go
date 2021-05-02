e0d773c02849152ca3021cd210a3b3fd48eed12d
Johann Tanzer <mail@johanntanzer.at>
2021-05-03 01:38:53 +0200

    stupid mistake

--- internal/tezosipfs/swarm/tezosSwarm.go
+++ internal/tezosipfs/swarm/tezosSwarm.go
@@ -14,20 +14,20 @@ import (
  * the nodes we care about
  */
 type Swarm struct {
-	trustedPeers []string // directly or 2nd level peers
-	knownPeers   map[string]*PeerAdvertisment
-	knownPeersTTL   map[string]time.Time // stores last update
-	cacheFor     []string
-	pinFor       []string
-	l            *sync.Mutex
-	log          *logrus.Entry
-	net          network.NetworkInterface
-	config       *config.Config
+	trustedPeers  []string // directly or 2nd level peers
+	knownPeers    map[string]*PeerAdvertisment
+	knownPeersTTL map[string]time.Time // stores last update
+	cacheFor      []string
+	pinFor        []string
+	l             *sync.Mutex
+	log           *logrus.Entry
+	net           network.NetworkInterface
+	config        *config.Config
 }
 
 func NewSwarm(c *config.Config, l *logrus.Entry, net network.NetworkInterface) *Swarm {
 	s := Swarm{}
-	s.log = l.WithField("source","swarm")
+	s.log = l.WithField("source", "swarm")
 	s.l = &sync.Mutex{}
 	s.knownPeers = map[string]*PeerAdvertisment{}
 	s.knownPeersTTL = map[string]time.Time{}
@@ -43,19 +43,19 @@ func NewSwarm(c *config.Config, l *logrus.Entry, net network.NetworkInterface) *
 	return &s
 }
 
-func (s *Swarm) updateConfig(c *config.Config){
+func (s *Swarm) updateConfig(c *config.Config) {
 	ch := c.GetUpdates()
 	for {
-		newConfig := <- ch
+		newConfig := <-ch
 		newaddrs := []string{}
-		for _,a := range newConfig.Peers.CacheFor {
-			newaddrs = append(s.trustedPeers, a)
+		for _, a := range newConfig.Peers.CacheFor {
+			newaddrs = append(newaddrs, a)
 		}
-		for _,a := range newConfig.Peers.PinFor {
-			newaddrs = append(s.trustedPeers, a)
+		for _, a := range newConfig.Peers.PinFor {
+			newaddrs = append(newaddrs, a)
 		}
-		for _,a := range newConfig.Peers.TrustedPeers {
-			newaddrs = append(s.trustedPeers, a)
+		for _, a := range newConfig.Peers.TrustedPeers {
+			newaddrs = append(newaddrs, a)
 		}
 
 		s.l.Lock()
@@ -70,13 +70,13 @@ func (s *Swarm) updateConfig(c *config.Config){
 
 func (s *Swarm) advertiseMyself() {
 	msg := PeerAdvertisment{
-		Name: s.config.Identity.Name,
+		Name:         s.config.Identity.Name,
 		Organization: s.config.Identity.Organization,
-		Contact: s.config.Identity.Contact,
-		Comment: s.config.Identity.Comment,
+		Contact:      s.config.Identity.Contact,
+		Comment:      s.config.Identity.Comment,
 	}
 	for {
-		time.Sleep(5*time.Second)
+		time.Sleep(5 * time.Second)
 		msg.TrustedPeers = s.config.Peers.TrustedPeers
 		msg.CacheFor = s.config.Peers.CacheFor
 		msg.PinFor = s.config.Peers.PinFor
@@ -84,27 +84,26 @@ func (s *Swarm) advertiseMyself() {
 	}
 }
 
-
-func (s *Swarm) connect(){
+func (s *Swarm) connect() {
 	s.l.Lock()
 	defer s.l.Unlock()
 	cp := s.trustedPeers
 	go s.net.Connect(cp)
 }
 
-func (s *Swarm) periodic(){
+func (s *Swarm) periodic() {
 	for {
-		time.Sleep(15*time.Second)
+		time.Sleep(15 * time.Second)
 		s.net.Connect(s.trustedPeers)
 	}
 }
 
-func (s *Swarm) purgePeers(){
+func (s *Swarm) purgePeers() {
 	// makes sure dead peers are removed from our list
 	timeout := 20 * time.Second
 	for {
 		time.Sleep(10 * time.Second)
-		for id,t := range s.knownPeersTTL {
+		for id, t := range s.knownPeersTTL {
 			if time.Now().Add(-1 * timeout).After(t) {
 				s.l.Lock()
 				delete(s.knownPeersTTL, id)
@@ -115,18 +114,18 @@ func (s *Swarm) purgePeers(){
 	}
 }
 
-func (s *Swarm) subscribe(){
+func (s *Swarm) subscribe() {
 	ch := s.net.Subscribe()
 	for {
-		msg := <- ch
+		msg := <-ch
 		if msg.Kind == "peer_advertisement" {
 			padv := PeerAdvertisment{}
-			json.Unmarshal(msg.Data,&padv)
+			json.Unmarshal(msg.Data, &padv)
 			padv.PeerId = msg.From
 			s.l.Lock()
 			s.knownPeers[padv.PeerId] = &padv
 			s.knownPeersTTL[padv.PeerId] = time.Now()
-			for _,trusted := range s.config.Peers.TrustedPeers {
+			for _, trusted := range s.config.Peers.TrustedPeers {
 				if trusted == padv.PeerId {
 					s.trustedPeers = append(s.trustedPeers, padv.TrustedPeers...)
 					s.trustedPeers = unique(s.trustedPeers)
@@ -149,11 +148,10 @@ func unique(stringSlice []string) []string {
 	return list
 }
 
-
 func (s *Swarm) PinFor(pid string) bool {
 	s.l.Lock()
 	defer s.l.Unlock()
-	for _,a := range s.pinFor {
+	for _, a := range s.pinFor {
 		if a == pid {
 			return true
 		}
@@ -164,7 +162,7 @@ func (s *Swarm) PinFor(pid string) bool {
 func (s *Swarm) CacheFor(pid string) bool {
 	s.l.Lock()
 	defer s.l.Unlock()
-	for _,a := range s.cacheFor {
+	for _, a := range s.cacheFor {
 		if a == pid {
 			return true
 		}
@@ -175,17 +173,17 @@ func (s *Swarm) CacheFor(pid string) bool {
 func (s *Swarm) IsTrusted(pid string) bool {
 	s.l.Lock()
 	defer s.l.Unlock()
-	for _,a := range s.trustedPeers {
+	for _, a := range s.trustedPeers {
 		if a == pid {
 			return true
 		}
 	}
-	for _,a := range s.cacheFor {
+	for _, a := range s.cacheFor {
 		if a == pid {
 			return true
 		}
 	}
-	for _,a := range s.pinFor {
+	for _, a := range s.pinFor {
 		if a == pid {
 			return true
 		}
@@ -196,8 +194,8 @@ func (s *Swarm) IsTrusted(pid string) bool {
 func (s *Swarm) CacheForUs() []*PeerAdvertisment {
 	res := []*PeerAdvertisment{}
 	id := s.net.ID()
-	for _,a := range s.knownPeers {
-		for _,b := range a.CacheFor {
+	for _, a := range s.knownPeers {
+		for _, b := range a.CacheFor {
 			if b == id {
 				res = append(res, a)
 			}
@@ -209,8 +207,8 @@ func (s *Swarm) CacheForUs() []*PeerAdvertisment {
 func (s *Swarm) PinForUs() []*PeerAdvertisment {
 	res := []*PeerAdvertisment{}
 	id := s.net.ID()
-	for _,a := range s.knownPeers {
-		for _,b := range a.PinFor {
+	for _, a := range s.knownPeers {
+		for _, b := range a.PinFor {
 			if b == id {
 				res = append(res, a)
 			}

bc9bcc4af09228b2e87a3ef4bbbcfd069219e0d1
Johann Tanzer <mail@johanntanzer.at>
2021-05-04 13:20:55 +0200

    ci

---
+++ ci.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+branch_name=$(git symbolic-ref --short -q HEAD)
+echo $branch_name
+ipfs_built="not yet"
+tipfs_built="not yet"
+
+build_ipfs(){
+  if [[ $ipfs_built == "yes" ]]; then
+    return
+  fi
+  echo "building IPFS node"
+  docker build -t tezoscommons/tezos-ipfs:ipfs-s3-$branch_name ./docker/ipfs-s3
+  docker push tezoscommons/tezos-ipfs:ipfs-s3-$branch_name
+  ipfs_built="yes"
+}
+
+build_tipfs(){
+  if [[ $tipfs_built == "yes" ]]; then
+    return
+  fi
+  docker build -t tezoscommons/tezos-ipfs:$branch_name ./
+  docker push tezoscommons/tezos-ipfs:$branch_name
+  tipfs_built = "yes"
+}
+
+// build ipfs on any branch containing ipfs
+if [[ $branch_name == *"ipfs"* ]]; then
+  build_ipfs
+fi
+
+// build app on any release branch
+if [[ $branch_name == "v"* ]]; then
+  tipfs_built
+fi
+
+if [[ $branch_name == "master" || $branch_name == "testing" ]]; then
+  tipfs_built
+fi
